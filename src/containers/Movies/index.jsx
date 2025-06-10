8be98877f0cf9e4cbb67b9c9763684afb63fc85f
Leonardo Pires <leopires1988@gmail.com>
2025-06-10 19:07:21 -0300

    Refactor code structure for improved readability and maintainability

---
+++ src/containers/Movies/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Movies() {
+  return (
+    <div>
+      <h1>Movies</h1>
+        <p>Here you can find a list of movies.</p>
+    </div>
+  );
+}
+
+export default Movies;
---
+++ src/containers/Series/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Series() {
+  return (
+    <div>
+      <h1>Series</h1>
+      <p>Here you can find a list of series.</p>
+    </div>
+  );
+}
+
+export default Series;
